5e3911f38c5788f115d6f9e61cd4bb7be6b799b6
Artur Motin <artur.motin98@gmail.com>
2023-01-04 19:59:38 +0200

    Icons, JS start

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+document.querySelector('#GPU').addEventListener('click', addGpu)
+
+addGpu()
+{
+    document.querySelector('#productList').innerHTML = '';
+    let productArray = [];
+    productArray.at(0) = 
+}
 
