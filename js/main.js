ddbed3b0c14fb8e92d79b875113810392d521959
Artur Motin <artur.motin98@gmail.com>
2023-01-07 20:56:10 +0200

    upd

--- js/main.js
+++ js/main.js
@@ -7,10 +7,55 @@
 //     productArray.at(0) = 0
 // }
 
-//add eventlsitener to "account" button, move to "account.html" page when clicked
-document.querySelector('#account').addEventListener('click', moveToAccount, false)
 
-moveToAccount()
+window.onload=function()
 {
-    //window.location.href("account.html")
+    //add eventlsitener to "account" button, move to "account.html" page when clicked
+    document.querySelector('#account').addEventListener('click', moveToAccount)
+    function moveToAccount()
+    {
+        window.open('account.html', '_self')
+    }
+    //add eventlistener to "gpu" button, clear the div in DOM that contais all products, print all GPU's
+    // document.querySelector('#GPU').addEventListener('click', getGpu)
+    // function getGpu()
+    // {
+    //     array of data that is needed for one product
+    //     let arr = ["images/account.png", "Name", "Price", "Description"];
+    // }
+
+    $("#productList").ready(async function() {
+        var cards = await getAllProducts();
+    
+        for (const index in cards) {
+            var card = cards[index];
+            $("#productList").append(card);
+        }
+    });
+    //https://ibb.co/vB83PzT
+    //ADD PRODUCT FROM ADDPRODUCT HTML PAGE
+    $("#productForm").submit(async function(form) {
+        form.preventDefault();
+
+        const name = $("#productNameInput").val();
+        const url = $("#productImageURLInput").val();
+        const description = $("#productDescriptionInput").val();
+        const price = $("#productPriceInput").val();
+
+        const product = {
+            "name": name,
+            "imageURL": url,
+            "description": description,
+            "price": price
+        };
+
+        var result = await createNewProduct(product);
+
+        if (result) {
+            alert("Product has been added!");
+            window.location.href = "index.html";
+        } else {
+            alert("Failed to add product, sorry");
+        }
+    });
 }
