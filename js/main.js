237c4656e263a7bf94005d27b8c5914887b0c2b6
Artur Motin <artur.motin98@gmail.com>
2023-01-07 22:47:33 +0200

    111

--- js/main.js
+++ js/main.js
@@ -19,52 +19,37 @@ window.onload=function()
     //add eventlistener to "gpu" button, clear the div in DOM that contais all products, print all GPU's
     document.querySelector('#GPU').addEventListener('click', getGpu)
     //array of data that is needed for one product
-    let arr = ["1", "https://ibb.co/vB83PzT", "Nvidia GTX950", "299€", "This is a Nvidia GPU"];
+    let arr = [["1", "images/GPU.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"], ["1", "images/CPU.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"], ["1", "images/RAM.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"]]
 
     function getGpu()
     {
-        let productId = arr[0]
-        // let productImage = arr[1]
-        // let productName = arr[2]
-        // let productPrice = arr[3]
-        // let productDescription = arr[4]
-        const card = document.createElement('div');
-        card.setAttribute('class', 'productCard')
-        card.setAttribute('id', productId)
-        document.querySelector('#productList').appendChild(card)
+        arr.forEach((attrs) => {
+            
 
-        let imageURL = arr[1]
-        const image = document.createElement('img');
-        image.setAttribute('class', 'productCard')
-        image.setAttribute('src', imageURL)
-        //'images/account.png'
-        document.querySelector('#productList').appendChild(image)
+            card = document.createElement('div')
+            img = document.createElement('img')
+            pname = document.createElement('span')
+            price = document.createElement('span')
+            description = document.createElement('p')
+    
+            card.classList.add('productCard')
+            img.classList.add('productImage')
+            pname.classList.add('productName')
+            price.classList.add('productPrice')
+            description.classList.add('productDescription')
+            
+            card.id = attrs[0]
+            img.src = attrs[1]
+            pname.innerText = attrs[2]
+            price.innerText = attrs[3]
+            description.innerText = attrs[4]
+    
+            card.appendChild(img)
+            card.appendChild(pname)
+            card.appendChild(price)
+            card.appendChild(description)
+    
+            document.querySelector('#productList').appendChild(card)
+        });
     }
-
 }
-
-// var product = products[index];
-        
-// var productID = product.id;
-// var productName = product.name;
-// var productImageURL = product.imageURL;
-// var productDescription = product.description;
-// var productPrice = product.price;
-
-// var card = `
-//     <div class="col-sm" id=${productID}>
-//         <div class="card bg-dark" style="width: 18rem;">
-//             <img class="card-img-top" src=${productImageURL} alt="Product Image">
-//             <div class="card-body text-white">
-//                 <h5 class="card-title">${productName}</h5>
-//                 <p class="card-text">${productDescription}</p>
-//                 <br />
-//                 <p><strong>Price: $</strong> ${productPrice}</p>
-//             </div>
-//             <div class="card-footer bg-transparent text-center row">
-//                 <button type="button" class="btn btn-outline-warning btn-sm col" id="buy-btn">Buy Product</button>
-//                 <button type="button" class="btn btn-outline-warning btn-sm col offset-md-1" id="edit-btn">Edit Product</button>
-//             </div>
-//         </div>
-//     </div>
-// `;
