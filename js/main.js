caf838f146e8ae471e541b94e8b33bb1ff1a914e
Artur Motin <artur.motin98@gmail.com>
2023-01-07 23:27:14 +0200

    11

--- js/main.js
+++ js/main.js
@@ -21,11 +21,52 @@ window.onload=function()
     //array of data that is needed for one product
     let arr = [["1", "images/GPU.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"], ["1", "images/CPU.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"], ["1", "images/RAM.png", "Nvidia GTX950", "299€", "This is a Nvidia GPU"]]
 
+
+
+    //CLEAR PRODUCT LIST PAGE
+    function clearList(){
+        const list = document.getElementById("productList")
+        list.innerHTML = ''
+        alert("Hello! I am an alert box!!");
+    }
+    //GET GPUS
+    function getGpu()
+    {
+        alert("Hello! I am an alert box!!");
+        clearList()
+        arr.forEach((attrs) => {
+            
+            card = document.createElement('div')
+            img = document.createElement('img')
+            pname = document.createElement('span')
+            price = document.createElement('span')
+            description = document.createElement('p')
+    
+            card.classList.add('productCard')
+            img.classList.add('productImage')
+            pname.classList.add('productName')
+            price.classList.add('productPrice')
+            description.classList.add('productDescription')
+            
+            card.id = attrs[0]
+            img.src = attrs[1]
+            pname.innerText = attrs[2]
+            price.innerText = attrs[3]
+            description.innerText = attrs[4]
+    
+            card.appendChild(img)
+            card.appendChild(pname)
+            card.appendChild(price)
+            card.appendChild(description)
+    
+            document.querySelector('#productList').appendChild(card)
+        });
+    }
+    //GET CPUS
     function getGpu()
     {
         arr.forEach((attrs) => {
             
-
             card = document.createElement('div')
             img = document.createElement('img')
             pname = document.createElement('span')
