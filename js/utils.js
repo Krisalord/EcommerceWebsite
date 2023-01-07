d8ccab0b279b0af284962de3d8857a815636a3d7
Artur Motin <artur.motin98@gmail.com>
2023-01-07 21:01:03 +0200

    1

--- js/utils.js
+++ js/utils.js
@@ -53,7 +53,7 @@ async function getAllProducts() {
 async function createNewProduct(product) {
     var result = false;
 
-    await fetch('http://remotedevs.org:8080/api/product/add/', {
+    await fetch('https://remotedevs.org:8080/api/product/add/', {
         method: 'POST',
         body: JSON.stringify(product),
         headers: {
