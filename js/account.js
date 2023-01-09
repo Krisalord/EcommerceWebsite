1e9bab7d6f06ce021726c7abf3e10914eeb1bfed
Artur Motin <artur.motin98@gmail.com>
2023-01-09 19:12:00 +0200

    update

--- js/account.js
+++ js/account.js
@@ -1,39 +1,40 @@
-let arr = [["1", "Artur", "Motin", "24.10.1998", "artur.motin98@gmail.com", "1234"], ["2", "Sanya", "Shelby", "14.4.2013", "sanya.kok@gmail.com", "1234"] ]
+let arr = [["1", "Artur", "Motin", "24.10.1998", "artur.motin98@gmail.com", "1234"]/*, ["2", "Sanya", "Shelby", "14.4.2013", "sanya.kok@gmail.com", "1234"]*/]
 
 // Load users data
-document.querySelector('#MyData').addEventListener('click', loadMyData)
-function loadMyData()
-{
-    document.querySelector('#content').innerHTML = ''
+// document.querySelector('#MyData').addEventListener('click', loadMyData)
+// function loadMyData()
+// {
+//     document.querySelector('#content').innerHTML = ''
 
-    arr.forEach((attrs) => {
-        MyDataDiv = document.createElement('div')
-        firstName = document.createElement('span')
-        familyName = document.createElement('span')
-        dateOfBirth = document.createElement('span')
-        email = document.createElement('span')
-        password = document.createElement('span')
+//     arr.forEach((attrs) => {
+//         let myDataDiv = document.createElement('div')
+//         myDataDiv.classList.add('myDataDiv')
 
-        MyDataDiv.classList.add('MyDataDiv')
-        firstName.classList.add('text')
-        familyName.classList.add('text')
-        dateOfBirth.classList.add('text')
-        email.classList.add('text')
-        password.classList.add('text')
-        
-        
-        firstName.innerHTML = attrs[0]
-        familyName.innerHTML = attrs[1]
-        dateOfBirth.innerHTML = attrs[2]
-        email.innerHTML = attrs[3]
-        password.innerHTML = attrs[4]
+//         let firstName = document.createElement('span')
+//         firstName.classList.add('myDataFirstName')
+//         firstName.innerText = attrs[1]
+//         myDataDiv.appendChild(firstName)
 
-        MyDataDiv.appendChild(firstName)
-        MyDataDiv.appendChild(familyName)
-        MyDataDiv.appendChild(dateOfBirth)
-        MyDataDiv.appendChild(email)
-        MyDataDiv.appendChild(password)
+//         let familyName = document.createElement('span')
+//         familyName.classList.add('myDataFamilyName')
+//         familyName.innerHTML = attrs[2]
+//         myDataDiv.appendChild(familyName)
 
-        document.querySelector('#content').appendChild(MyDataDiv)
-    });
-}
+//         let dateOfBirth = document.createElement('span')
+//         dateOfBirth.classList.add('myDataDateOfBirth')
+//         dateOfBirth.innerHTML = attrs[3]
+//         myDataDiv.appendChild(dateOfBirth)
+
+//         let email = document.createElement('span')
+//         email.classList.add('myDataEmail')
+//         email.innerHTML = attrs[4]
+//         myDataDiv.appendChild(email)
+
+//         let password = document.createElement('span')
+//         password.classList.add('text')
+//         password.innerHTML = attrs[5]
+//         myDataDiv.appendChild(password)
+
+//         document.querySelector('#content').appendChild(myDataDiv)
+//     });
+// }
